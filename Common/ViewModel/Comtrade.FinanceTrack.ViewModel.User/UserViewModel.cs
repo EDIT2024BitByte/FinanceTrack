d5d8e9676200e971f4f64efddaaf15b04e3a5d37
EDIT2024BitByte <edit6185@gmail.com>
2024-07-19 18:16:14 +0200

    user view model add

---
+++ Common/ViewModel/Comtrade.FinanceTrack.ViewModel.User/UserViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace Comtrade.FinanceTrack.ViewModel.User
+{
+    public class UserViewModel
+    {
+        public long Id { get; set; }
+        public string? Username { get; set; }
+        public string? Password { get; set; }
+        public string? Firstname { get; set; }
+        public string? Lastname { get; set; }
+        public bool IsDeleted { get; set; }
+    }
+}
