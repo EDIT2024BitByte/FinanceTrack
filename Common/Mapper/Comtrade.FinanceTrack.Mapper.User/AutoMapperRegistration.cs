544f106902547a6ffcfba6d7650683b571a3f3dd
EDIT2024BitByte <edit6185@gmail.com>
2024-07-19 18:20:16 +0200

    user mapper

---
+++ Common/Mapper/Comtrade.FinanceTrack.Mapper.User/AutoMapperRegistration.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using Comtrade.FinanceTrack.ViewModel.User;
+
+namespace Comtrade.FinanceTrack.Mapper.User
+{
+    public class UserMappingProfile : Profile
+    {
+        public UserMappingProfile()
+        {
+            CreateMap<Comtrade.FinanceTrack.User.Repository.MSSQL.Models.User, UserViewModel>().ReverseMap();
+        }
+    }
+}
